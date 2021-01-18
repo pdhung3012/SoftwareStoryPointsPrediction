1bf35c9af1a9d43dcdd642b91f37e916b2b3598b
pdhung3012 <pdhung3012@gmail.com>
2021-01-17 23:56:41 -0600

    update TFIDF results

---
+++ devItems/spring-2021/UtilFunctions.py
@@ -0,0 +1,9 @@
+import os
+
+def createDirIfNotExist(fopOutput):
+    try:
+        # Create target Directory
+        os.makedirs(fopOutput, exist_ok=True)
+        print("Directory ", fopOutput, " Created ")
+    except FileExistsError:
+        print("Directory ", fopOutput, " already exists")
