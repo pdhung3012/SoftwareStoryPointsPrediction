96f768b852d4f608093cf5006e90063aceef6e17
hungphd <hungphd@iastate.edu>
2021-03-17 18:10:42 -0500

    update

--- devItems/spring-2021/analysis/RQ4_textPOS_lsvr_dimensions.py
+++ devItems/spring-2021/analysis/RQ4_textPOS_lsvr_dimensions.py
@@ -74,9 +74,9 @@ for i in range(0,len(list_files)):
         strContent =' '.join([str(columnTitle[j]),str(columnDescription[j])])
         # strContent = ' '.join([str(columnDescription[j])])
         strContent=preprocessTextV3(strContent,ps,lemmatizer)
-        intValue=int(columnSP[j])
-        if intValue>=40:
-            continue
+        # intValue=int(columnSP[j])
+        # if intValue>=40:
+        #     continue
 
         # print(strContent)
         lstTexts.append(strContent)
