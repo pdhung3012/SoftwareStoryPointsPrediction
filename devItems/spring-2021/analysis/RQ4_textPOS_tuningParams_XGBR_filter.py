14c58e09bfee419bdc5c73e3d5c79f380d34f871
hungphd <hungphd@iastate.edu>
2021-03-21 14:37:53 -0500

    update

--- devItems/spring-2021/analysis/RQ4_textPOS_tuningParams_XGBR_filter.py
+++ devItems/spring-2021/analysis/RQ4_textPOS_tuningParams_XGBR_filter.py
@@ -75,155 +75,153 @@ lstAvgTuneMAE=[]
 for mIndex in range(0, len(param_grid['max_depth'])):
     for mcwIndex in range(0, len(param_grid['min_child_weight'])):
         for etaIndex in range(0, len(param_grid['eta'])):
-            for alpIndex in range(0, len(param_grid['alpha'])):
-                for lrIndex in range(0, len(param_grid['learning_rate'])):
-                    lstMAE = []
-                    lstValMAE = []
-                    valMItem=param_grid['max_depth'][mIndex]
-                    valMcwItem=param_grid['min_child_weight'][mcwIndex]
-                    valEtaItem = param_grid['eta'][etaIndex]
+            lstMAE = []
+            lstValMAE = []
+            valMItem=param_grid['max_depth'][mIndex]
+            valMcwItem=param_grid['min_child_weight'][mcwIndex]
+            valEtaItem = param_grid['eta'][etaIndex]
 
 
-                    countBeaten=0
-                    for i in range(0,len(list_files)):
-                        fileName=list_files[i]
-                        systemName=fileName.replace('.csv','')
-                        fpSystemCsv=fopDataset+fileName
-                        dfSystem=pd.read_csv(fpSystemCsv)
-                        priorI=lstPrior[i]
-                        fpItemText = fopOutputAllSystems + systemName + '_text.txt'
-                        fpItemLabel = fopOutputAllSystems + systemName + '_label.txt'
-                        fpVectorItemReg=fopOutputAllSystems+systemName+'_vector.csv'
+            countBeaten=0
+            for i in range(0,len(list_files)):
+                fileName=list_files[i]
+                systemName=fileName.replace('.csv','')
+                fpSystemCsv=fopDataset+fileName
+                dfSystem=pd.read_csv(fpSystemCsv)
+                priorI=lstPrior[i]
+                fpItemText = fopOutputAllSystems + systemName + '_text.txt'
+                fpItemLabel = fopOutputAllSystems + systemName + '_label.txt'
+                fpVectorItemReg=fopOutputAllSystems+systemName+'_vector.csv'
 
-                        lstTexts = []
-                        lstLabels = []
-                        if (isUseBackup and os.path.exists(fpItemText)):
-                            fff = open(fpItemText, 'r')
-                            lstTexts = fff.read().strip().split('\n')
-                            fff.close()
-                            fff = open(fpItemLabel, 'r')
-                            arrLbls = fff.read().strip().split('\n')
-                            for item in arrLbls:
-                                lstLabels.append(int(item))
-                            fff.close()
-                        else:
-                            projectName=systemName.split('_')[3].lower()
-                            dfWords = pd.read_excel(fpRQ2PerProject, sheet_name=projectName)
-                            lstFilterWords = []
-                            columnCount = dfWords['Count']
-                            columnUniqueWords = dfWords['Text']
-                            threshold = 30
-                            for index in range(0, len(columnUniqueWords)):
-                                itemCount = columnCount[index]
-                                itemWord = str(columnUniqueWords[index])
-                                '''
-                                arrWs=itemWord.split('---')
-                                if(len(arrWs)<2):
-                                    continue
-                                '''
-                                if itemCount <= 1:
-                                    lstFilterWords.append(itemWord)
-
-                            setFilterWords = set(lstFilterWords)
-
-                            lstTexts=[]
-                            lstLabels=[]
-                            columnTitle=dfSystem['title']
-                            columnDescription=dfSystem['description']
-                            columnSP=dfSystem['storypoint']
-
-                            for j in range(0,len(columnTitle)):
-                                strContent =' '.join([str(columnTitle[j]),str(columnDescription[j])])
-                                # strContent = ' '.join([str(columnDescription[j])])
-                                strContent=preprocessTextV3_FilerWordAndReplace(strContent,setFilterWords,ps,lemmatizer)
-                                # intValue=int(columnSP[j])
-                                # if intValue>=40:
-                                #     continue
-
-                                # print(strContent)
-                                lstTexts.append(strContent)
-                                lstLabels.append(columnSP[j])
-                            fff = open(fpItemText, 'w')
-                            fff.write('\n'.join(lstTexts))
-                            fff.close()
-                            fff = open(fpItemLabel, 'w')
-                            fff.write('\n'.join(map(str, lstLabels)))
-                            fff.close()
-
-                        if not os.path.exists(fpVectorItemReg):
-                            vectorizer = TfidfVectorizer(ngram_range=(1,1))
-                            X = vectorizer.fit_transform(lstTexts)
-                            X = X.toarray()
-                            print('Numbers of n-grams: {}'.format(len(X[0])))
-                            # X = PCA().fit(X)
-                            pca = PCA(n_components=100)
-                            X = pca.fit_transform(X)
-
-
-                            lenVectorOfWord = len(X[0])
-                            columnTitleRow = "no,story,"
-                            for j in range(0, lenVectorOfWord):
-                                item = 'feature-' + str(j + 1)
-                                columnTitleRow = ''.join([columnTitleRow, item])
-                                if j != lenVectorOfWord - 1:
-                                    columnTitleRow = ''.join([columnTitleRow, ","])
-                            columnTitleRow = ''.join([columnTitleRow, "\n"])
-                            csv = open(fpVectorItemReg, 'w')
-                            csv.write(columnTitleRow)
-                            corpusVector = []
-                            for j in range(0, len(lstTexts)):
-                                vector = X[j]
-                                corpusVector.append(vector)
-                                strReg = str(lstLabels[j])
-                                strRow2 = ''.join([str(j + 1), ',', '' + strReg, ])
-                                for k in range(0, lenVectorOfWord):
-                                    strRow2 = ''.join([strRow2, ',', str(vector[k])])
-
-                                strRow2 = ''.join([strRow2, '\n'])
-                                #   csv.write(strRow)
-                                csv.write(strRow2)
-                            csv.close()
-
-                        dfVectors=pd.read_csv(fpVectorItemReg)
-                        all_label = dfVectors['story']
-                        all_data = dfVectors
-                        #all_data = dfVectors.drop(['no', 'story'], axis=1)
-
-
-
-                        X_train, X_test, y_train, y_test = train_test_split(all_data, all_label, test_size = 0.2, shuffle=False)
-
-                       # # print('{}\t{}'.format(type(X_train),type(y_train)))
-                        X_train=X_train[X_train['story']<=20]
-                        y_train = X_train['story']
-                        X_train=X_train.drop(['no', 'story'], axis=1)
-                        X_test=X_test.drop(['no', 'story'], axis=1)
-
-
-                        regressor=XGBRegressor(max_depth=valMItem,min_child_weight=valMcwItem,eta=valEtaItem)
-                        regressor.fit(X_train, y_train)
-                        predicted = regressor.predict(X_test)
-                        maeAccuracy = mean_absolute_error(y_test, predicted)
-                        # strAcc='{}\t{}'.format(systemName,maeAccuracy)
-                        strAcc = '{}'.format(maeAccuracy)
-                        lstMAE.append(strAcc)
-                        lstValMAE.append(maeAccuracy)
-                        if maeAccuracy<priorI:
-                            countBeaten=countBeaten+1
-                        print('Finish {}'.format(systemName))
-
-                    from statistics import mean
-                    avgValue=mean(lstValMAE)
-                    fnTuneName='-'.join([str(valMItem),str(valMcwItem),str(valEtaItem)])
-                    newTuple=(fnTuneName,avgValue)
-                    lstAvgTuneMAE.append(newTuple)
-
-                    #lstMAE.append('Average\t{}'.format(avgValue))
-                    lstMAE.append('{}\n{}'.format(avgValue,countBeaten))
-                    fpRegressionResult=fopResultTuning+fnTuneName+'.txt'
-                    fff=open(fpRegressionResult,'w')
-                    fff.write('\n'.join(lstMAE))
+                lstTexts = []
+                lstLabels = []
+                if (isUseBackup and os.path.exists(fpItemText)):
+                    fff = open(fpItemText, 'r')
+                    lstTexts = fff.read().strip().split('\n')
                     fff.close()
+                    fff = open(fpItemLabel, 'r')
+                    arrLbls = fff.read().strip().split('\n')
+                    for item in arrLbls:
+                        lstLabels.append(int(item))
+                    fff.close()
+                else:
+                    projectName=systemName.split('_')[3].lower()
+                    dfWords = pd.read_excel(fpRQ2PerProject, sheet_name=projectName)
+                    lstFilterWords = []
+                    columnCount = dfWords['Count']
+                    columnUniqueWords = dfWords['Text']
+                    threshold = 30
+                    for index in range(0, len(columnUniqueWords)):
+                        itemCount = columnCount[index]
+                        itemWord = str(columnUniqueWords[index])
+                        '''
+                        arrWs=itemWord.split('---')
+                        if(len(arrWs)<2):
+                            continue
+                        '''
+                        if itemCount <= 1:
+                            lstFilterWords.append(itemWord)
+
+                    setFilterWords = set(lstFilterWords)
+
+                    lstTexts=[]
+                    lstLabels=[]
+                    columnTitle=dfSystem['title']
+                    columnDescription=dfSystem['description']
+                    columnSP=dfSystem['storypoint']
+
+                    for j in range(0,len(columnTitle)):
+                        strContent =' '.join([str(columnTitle[j]),str(columnDescription[j])])
+                        # strContent = ' '.join([str(columnDescription[j])])
+                        strContent=preprocessTextV3_FilerWordAndReplace(strContent,setFilterWords,ps,lemmatizer)
+                        # intValue=int(columnSP[j])
+                        # if intValue>=40:
+                        #     continue
+
+                        # print(strContent)
+                        lstTexts.append(strContent)
+                        lstLabels.append(columnSP[j])
+                    fff = open(fpItemText, 'w')
+                    fff.write('\n'.join(lstTexts))
+                    fff.close()
+                    fff = open(fpItemLabel, 'w')
+                    fff.write('\n'.join(map(str, lstLabels)))
+                    fff.close()
+
+                if not os.path.exists(fpVectorItemReg):
+                    vectorizer = TfidfVectorizer(ngram_range=(1,1))
+                    X = vectorizer.fit_transform(lstTexts)
+                    X = X.toarray()
+                    print('Numbers of n-grams: {}'.format(len(X[0])))
+                    # X = PCA().fit(X)
+                    pca = PCA(n_components=100)
+                    X = pca.fit_transform(X)
+
+
+                    lenVectorOfWord = len(X[0])
+                    columnTitleRow = "no,story,"
+                    for j in range(0, lenVectorOfWord):
+                        item = 'feature-' + str(j + 1)
+                        columnTitleRow = ''.join([columnTitleRow, item])
+                        if j != lenVectorOfWord - 1:
+                            columnTitleRow = ''.join([columnTitleRow, ","])
+                    columnTitleRow = ''.join([columnTitleRow, "\n"])
+                    csv = open(fpVectorItemReg, 'w')
+                    csv.write(columnTitleRow)
+                    corpusVector = []
+                    for j in range(0, len(lstTexts)):
+                        vector = X[j]
+                        corpusVector.append(vector)
+                        strReg = str(lstLabels[j])
+                        strRow2 = ''.join([str(j + 1), ',', '' + strReg, ])
+                        for k in range(0, lenVectorOfWord):
+                            strRow2 = ''.join([strRow2, ',', str(vector[k])])
+
+                        strRow2 = ''.join([strRow2, '\n'])
+                        #   csv.write(strRow)
+                        csv.write(strRow2)
+                    csv.close()
+
+                dfVectors=pd.read_csv(fpVectorItemReg)
+                all_label = dfVectors['story']
+                all_data = dfVectors
+                #all_data = dfVectors.drop(['no', 'story'], axis=1)
+
+
+
+                X_train, X_test, y_train, y_test = train_test_split(all_data, all_label, test_size = 0.2, shuffle=False)
+
+               # # print('{}\t{}'.format(type(X_train),type(y_train)))
+                X_train=X_train[X_train['story']<=20]
+                y_train = X_train['story']
+                X_train=X_train.drop(['no', 'story'], axis=1)
+                X_test=X_test.drop(['no', 'story'], axis=1)
+
+
+                regressor=XGBRegressor(max_depth=valMItem,min_child_weight=valMcwItem,eta=valEtaItem)
+                regressor.fit(X_train, y_train)
+                predicted = regressor.predict(X_test)
+                maeAccuracy = mean_absolute_error(y_test, predicted)
+                # strAcc='{}\t{}'.format(systemName,maeAccuracy)
+                strAcc = '{}'.format(maeAccuracy)
+                lstMAE.append(strAcc)
+                lstValMAE.append(maeAccuracy)
+                if maeAccuracy<priorI:
+                    countBeaten=countBeaten+1
+                print('Finish {}'.format(systemName))
+
+            from statistics import mean
+            avgValue=mean(lstValMAE)
+            fnTuneName='-'.join([str(valMItem),str(valMcwItem),str(valEtaItem)])
+            newTuple=(fnTuneName,avgValue)
+            lstAvgTuneMAE.append(newTuple)
+
+            #lstMAE.append('Average\t{}'.format(avgValue))
+            lstMAE.append('{}\n{}'.format(avgValue,countBeaten))
+            fpRegressionResult=fopResultTuning+fnTuneName+'.txt'
+            fff=open(fpRegressionResult,'w')
+            fff.write('\n'.join(lstMAE))
+            fff.close()
 
 sortTuple(lstAvgTuneMAE,False)
 fpTotalResult=fopOutputAllSystems+'result.txt'
