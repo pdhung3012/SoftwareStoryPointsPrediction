918f2f3d0ba003c6251393c21a7b4cf6332904fa
pdhung3012 <pdhung3012@gmail.com>
2021-03-22 22:57:05 -0500

    up

--- devItems/spring-2021/analysis/RQ4_textPOS_regradeNullLbl.py
+++ devItems/spring-2021/analysis/RQ4_textPOS_regradeNullLbl.py
@@ -30,7 +30,7 @@ sys.path.append('../')
 from UtilFunctions import *
 
 fopOutput='../../../../dataPapers/analysisSEE/'
-fopOutputAllSystems=fopOutput+'/RQ4_analyzeGap/'
+fopOutputAllSystems=fopOutput+'/RQ4_regradeNull/'
 fopResultTuning=fopOutputAllSystems+'/result_tuning/'
 fopDataset='../../dataset_sorted/'
 isUseBackup=True
